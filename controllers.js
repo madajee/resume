e37d2c803139096985dd0b70e237de1df4c62e77
Jeetan Madaan <madajeeblog@gmail.com>
2020-09-30 11:42:05 +0530

    eleventh commit

--- controllers.js
+++ controllers.js
@@ -1,5 +1,12 @@
 app.controller('homeController', ['$scope',
   function($scope) {
+    $scope.randomimage = "https://source.unsplash.com/random/400x300";
+    $scope.input = 300;
+    $scope.reload = function() {
+        $scope.input = $scope.input + 1;
+        $scope.randomimage = 'https://source.unsplash.com/random/400x' + $scope.input;
+
+    };
   }]);
 
 app.controller('projectController', ['$scope',
