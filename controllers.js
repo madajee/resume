92a943fac7f7ac55d7303e35aa4a5ff5f0ee4c5e
Jeetan Madaan <madajeeblog@gmail.com>
2020-12-23 20:55:29 +0530

    fourteen commit

--- controllers.js
+++ controllers.js
@@ -21,11 +21,12 @@ app.controller('testimonialController', ['$scope',
       'https://udemy-certificate.s3.amazonaws.com/image/UC-O6Z2ZF66.jpg',
       'https://res.cloudinary.com/madajee/image/upload/v1600437489/Certifications/PSM-I.png',
       'https://udemy-certificate.s3.amazonaws.com/image/UC-KRWDC2Y4.jpg',
-      'https://res.cloudinary.com/madajee/image/upload/v1600436743/Certifications/M001-MongoDB-Basics.png',
+      'https://res.cloudinary.com/madajee/image/upload/v1608729296/Certifications/M220JS-MongoDB_For_JS_Developers.jpg',
       'https://res.cloudinary.com/madajee/image/upload/v1600437754/Certifications/M103_-_Course_Completion_Confirmation.jpg',
       'https://res.cloudinary.com/madajee/image/upload/v1600476853/Certifications/Salesforce-PD-I.png',
       'https://res.cloudinary.com/madajee/image/upload/v1600477117/Certifications/M121-Aggregation-Framework.png',
-      'https://udemy-certificate.s3.amazonaws.com/image/UC-ae18f3e3-1ce5-4a7c-b61c-1d41e929f21e.jpg'
+      'https://udemy-certificate.s3.amazonaws.com/image/UC-ae18f3e3-1ce5-4a7c-b61c-1d41e929f21e.jpg',
+      'https://res.cloudinary.com/madajee/image/upload/v1600436743/Certifications/M001-MongoDB-Basics.png'
     ];
 }]);
 app.controller('toolsController', ['$scope',
